47903788cbe68021209f3bcf458a692f4ee86198
Lutz <lutz.kuenneke89@gmail.com>
2019-07-17 20:26:22 +0000

    initial commit

---
+++ pip_upgrade_all.py
@@ -0,0 +1,10 @@
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+for package in packages:
+ try:
+  call("sudo python3 -m pip install --upgrade " + package + ' --user', shell=True)
+ except Exception as e:
+  print(str(e))
+
---
+++ setup_postgres_airflow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# set up a postgresql installation of airflow
+
+sudo apt-get update --fix-missing
+sudo apt-get upgrade
+
+
