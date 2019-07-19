aef21eb10f2ae4b9a619ba5496c6aae6551de141
LutzKuen <lutz.kuenneke89@gmail.com>
2019-07-19 04:25:16 +0000

    init git

--- init_git.sh
+++ init_git.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git config --global --edit
+git config --global user.name 'LutzKuen'
+git config --global user.email 'lutz.kuenneke89@gmail.com'
+echo "Username"
+git config --global --get user.name
+echo "Mail"
+git config --global --get user.email
