49d4e9f101aeded13908fdae7e618ef2b0370de8
lutzkuen <lutz.kuenneke89@gmail.com>
2019-07-20 10:40:55 +0200

    Added pip upgrade for win

---
+++ pip_upgrade_all_win.py
@@ -0,0 +1,10 @@
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+for package in packages:
+ try:
+  call("python -m pip install --upgrade " + package + ' --user', shell=True)
+ except Exception as e:
+  print(str(e))
+
